9b9cf092c04171bceb6d6acc471c55a5ccd45cf4
tanipa1 <tania35.291@gmail.com>
2023-04-30 21:24:45 +0600

    News Data

--- index.js
+++ index.js
@@ -3,15 +3,38 @@ const app = express();
 cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 app.get('/categories', (req, res) => {
   res.send(categories)
+})
+
+app.get('/categories/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  if (id === 0) {
+    res.send(news)
+  }
+  else {
+    const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+    res.send(categoryNews)
+  }
+})
+
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
 })
 
 app.listen(port, () => {
