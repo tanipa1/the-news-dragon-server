1e2e2b7d8c90be8ceb21344c6cdeabb22d7fca82
tanipa1 <tania35.291@gmail.com>
2023-04-30 19:24:00 +0600

    Data

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/categories', (req, res) => {
+  res.send(categories)
+})
+
+app.listen(port, () => {
+  console.log(`Dragon is listening on port ${port}`)
+})
